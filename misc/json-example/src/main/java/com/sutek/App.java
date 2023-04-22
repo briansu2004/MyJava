e9c91ab3977f6d9c041964c7c64451a3f975c29d
Brian Su <briansu2004@hotmail.com>
2023-04-22 15:37:45 -0400

    $

--- misc/json-example/src/main/java/com/sutek/App.java
+++ misc/json-example/src/main/java/com/sutek/App.java
@@ -3,11 +3,13 @@ package com.sutek;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.github.fge.jsonpatch.JsonPatchException;
+import com.github.fge.jsonpatch.mergepatch.JsonMergePatch;
 import java.io.IOException;
 import java.io.InputStream;
 
 public class App {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, JsonPatchException {
         InputStream jsonInput = App.class.getResourceAsStream("/example.json");
 
         ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
@@ -16,9 +18,40 @@ public class App {
 
         Person person = objectMapper.treeToValue(jsonNode, Person.class);
 
+        System.out.println("(Parsing JSON)");
         System.out.println("Name: " + person.getName());
         System.out.println("Age: " + person.getAge());
         System.out.println("Email: " + person.getEmail());
         System.out.println("BirthDate: " + person.getBirthDate());
+
+
+        System.out.println("");
+        System.out.println("(JsonMergePatching)");
+
+
+        // Define the original JSON object
+        String originalJson = "{ \"name\": \"John\", \"age\": 30 }";
+        System.out.println("Original JSON: " + originalJson);
+
+        // Define the patch to be applied
+        String patchJson = "{ \"age\": 31 }";
+        System.out.println("Patch JSON: " + patchJson);
+
+        // Create an ObjectMapper to read the patch JSON into a JsonNode
+        // ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode patchNode = objectMapper.readTree(patchJson);
+
+        // Create a JsonMergePatch object from the patch JsonNode
+        JsonMergePatch patch = JsonMergePatch.fromJson(patchNode);
+
+        // Read the original JSON into a JsonNode
+        JsonNode originalNode = objectMapper.readTree(originalJson);
+
+        // Apply the patch to the original JSON object
+        JsonNode updatedNode = patch.apply(originalNode);
+
+        // Convert the updated JsonNode back to a JSON string and print it to the console
+        String updatedJson = objectMapper.writeValueAsString(updatedNode);
+        System.out.println("Updated JSON: " + updatedJson);
     }
 }
