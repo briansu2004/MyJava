8a6075aa174c6dd1056b392a8f3ade3679f5ef9f
Brian Su <briansu2004@hotmail.com>
2023-09-03 08:41:39 -0400

    $

---
+++ misc/IfElse/IfElseHelloWorld.java
@@ -0,0 +1,9 @@
+public class IfElseHelloWorld {
+    public static void main(String[] args) {
+        if (System.out.append("hello ") == null) {
+            System.out.print("hello ");
+        } else {
+            System.out.print("world");
+        }
+    }
+}
