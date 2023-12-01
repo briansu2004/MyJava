72d9a4aaf8524ec33599d79b0f6a8bbdc044ffef
Brian Su <6775994+briansu2004@users.noreply.github.com>
2023-12-01 13:11:53 -0500

    $

--- misc/enum/enumProvince/src/main/java/com/sutek/enumexample/province/App.java
+++ misc/enum/enumProvince/src/main/java/com/sutek/enumexample/province/App.java
@@ -1,9 +1,11 @@
 package com.sutek.enumexample.province;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import com.sutek.enumexample.province.bo.Provider;
 import com.sutek.enumexample.province.bo.UIN;
+import com.sutek.enumexample.province.util.enums.Province;
 
 /**
  * Hello world!
@@ -11,16 +13,26 @@ import com.sutek.enumexample.province.bo.UIN;
  */
 public class App {
     public static void main(String[] args) {
+        List<Province> provinceList = retrieveProvinces();
 
-        getUIN();
+        Provider provider = getMockProvider();
+
+        transformUIN(provider, provinceList);
 
         System.out.println("Nice!");
     }
 
+    private static List<Province> retrieveProvinces() {
+        List<Province> provinceList = new ArrayList<>();
+        Collections.addAll(provinceList, Province.values());
+        return provinceList;
+    }
 
-    public static void getUIN() {
+    private static Provider getMockProvider() {
+        Provider provider = new Provider();
+
         // mock data
-        UIN u1 = new UIN("AB", "ON", "123");
+        UIN u1 = new UIN("XY", "ON", "123");
         UIN u2 = new UIN("GH", "QC", "5436");
         UIN u3 = new UIN("KL", "AB", "33455");
 
@@ -29,11 +41,21 @@ public class App {
         mockUINs.add(u2);
         mockUINs.add(u3);
 
-        Provider provider = new Provider();
         provider.setUin(mockUINs);
 
-        // business logic
+        return provider;
+    }
+
+    private static void transformUIN(Provider provider, List<Province> provinceList) {
         List<UIN> lstUIN = provider.getUin();
+
+        // Update province values based on provinceList
+        for (UIN uin : lstUIN) {
+            Province matchingProvince = findProvinceByCode(provinceList, uin.getProvince());
+            if (matchingProvince != null) {
+                uin.setProvince(matchingProvince.getName());
+            }
+        }
 
         // Using forEach with lambda expression to print each element
         // lstUIN.forEach(uin -> System.out.println(uin));
@@ -41,5 +63,14 @@ public class App {
             String provinceName = uin.getProvince();
             System.out.println("UIN: " + uin + ", Province Name: " + provinceName);
         });
+    }
+
+    private static Province findProvinceByCode(List<Province> provinceList, String code) {
+        for (Province province : provinceList) {
+            if (province.name().equalsIgnoreCase(code)) {
+                return province;
+            }
+        }
+        return null;
     }
 }
